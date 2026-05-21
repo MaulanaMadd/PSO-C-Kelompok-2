51139ed1f137b22b2c514e686fb70ad247aa7ddb
MaulanaMadd <djikodotkom@gmail.com>
2026-05-21 16:49:29 +0700

    vite.config.js update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: ['my-app-service-190501588992.asia-southeast2.run.app']
+  }
 })
