a68854ed3c59c256c7ad5f5cd25792096699feb8
NopalErwin <162094876+NopalErwin@users.noreply.github.com>
2026-06-10 11:03:16 +0700

    trigger test

--- backend/tests/test_main.py
+++
@@ -1,8 +0,0 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-def test_backend_app_running():
-    response = client.get("/")
-    assert response.status_code in [200, 404]
